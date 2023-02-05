2c0f7d2b385d9fe422b12d62553acf79fa36c72e
Ye Gaung Kyaw <31139816+hikariakio@users.noreply.github.com>
2023-02-05 16:53:35 +1000

    SeedOrder Added

--- GlobalGameJam2023/Assets/DropSeed.cs
+++ GlobalGameJam2023/Assets/DropSeed.cs
@@ -11,8 +11,24 @@ public class DropSeed : MonoBehaviour
     [SerializeField] Planter planter;
 
     public bool IsHidden = false;
+    public string seedName;
     public void ActivateIcon()
     {
+        GameManager.Instance.PlayerController.IsTeleporting = true;
         planter.ShowInteractIcon();
+        UIManager.Instance.DisplaySpeech( string.Format("You acquired some \"{0}\" seeds",seedName));
+        StartCoroutine(HideSpeech(1.2f));
     }
+
+    public IEnumerator HideSpeech(float t)
+    {
+        yield return new WaitForSeconds(t);
+        GameManager.Instance.PlayerController.IsTeleporting = false;
+
+        UIManager.Instance.HideSpeech();
+        gameObject.SetActive(false);
+
+    }
+
+
 }
--- GlobalGameJam2023/Assets/_Scripts/UIManager.cs
+++ GlobalGameJam2023/Assets/_Scripts/UIManager.cs
@@ -44,4 +44,6 @@ public class UIManager : MonoBehaviour
         SpeechTextBox.SetActive(false);
         IsDisplayed = false;
     }
+
+    
 }
